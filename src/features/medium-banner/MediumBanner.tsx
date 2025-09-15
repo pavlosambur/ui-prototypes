a4cf533c890b534d7c70b9b5aade48c3bf9eac38
Pavlo Sambur <samburp@gmail.com>
2025-09-15 18:13:43 +0300

    config

---
+++ src/features/medium-banner/MediumBanner.tsx
@@ -0,0 +1,11 @@
+const MediumBanner = () => {
+  return (
+    <>
+      <div className="font-roboto">MediumBanner</div>
+      <div className="bg-amber-300">MediumBanner</div>
+      <div className="bg-testbg text-white p-4">Tailwind бачить мій config 🎉</div>
+    </>
+  );
+};
+
+export default MediumBanner;
